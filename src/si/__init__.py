1e11d4110818882d6c0beee2c8623f0c559fe77f
AngelinaEiras <angie.eiras@gmail.com>
2022-10-10 11:49:15 +0100

    autoria

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,5 +1,5 @@
-__author__ = "Fernando Cruz"
-__credits__ = ["Fernando Cruz"]
+__author__ = "Angelina Eiras"
+__credits__ = ["Angelina Eiras"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
 __maintainer__ = "Fernando Cruz"
