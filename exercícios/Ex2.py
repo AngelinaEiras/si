aae10520d6de0c4b778d1070427def0abba3596c
AngelinaEiras <angie.eiras@gmail.com>
2022-10-10 12:18:00 +0100

    .

---
+++ exercícios/Ex2.py
@@ -0,0 +1,14 @@
+'''
+Exercício 2: NumPy array Indexing/Slicing
+• 2.1) Adiciona um método ao objeto Dataset que remove todas as amostras que contêm pelo menos um valor nulo (NaN). Nota que o objeto resultante não deve conter valores nulos em para nenhuma feature/variável independente. Nota também que deves atualizar o vetor y removendo as entradas associadas às amostras a remover.
+Deves usar apenas funções no NumPy.
+Nome do método: dropna
+
+• 2.2) Adiciona um método ao objeto Dataset que substitui todas os valores nulos por outro valor (argumento da função/método). Nota que o objeto resultante não deve conter valores nulos em para nenhuma feature/variável independente. 
+Deves usar apenas funções no NumPy.
+Nome do método: fillna
+• Opcional: Podes adicionar exemplos de como utilizar estes métodos à script do exercício 1.
+'''
+
+#Exercício no script dataset
+
