70e2e9754686d7e58ffe02adc9a1813e93116285
AngelinaEiras <angie.eiras@gmail.com>
2022-12-15 23:02:49 +0000

    aula5

---
+++ src/si/linear_model/logistic_regression.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+from si.data.dataset import Dataset
+from si.metrics.mse import mse
+
+
+class LogisticRegression:
+
+    def __init__(self, l2_penalty: float = 1, alpha: float = 0.001, max_iter: int = 1000) -> None:
+
+    # parameters
+        self.l2_penalty = l2_penalty
+        self.alpha = alpha
+        self.max_iter = max_iter
+
+        # attributes
+        self.theta = None
+        self.theta_zero = None
+    
+    def fit(self, dataset: Dataset) -> 'LogisticRegression':
+
+        m, n = dataset.shape()
+
+        # initialize the model parameters
+        self.theta = np.zeros(n)
+        self.theta_zero = 0
+
+        # gradient descent
+        for i in range(self.max_iter):
+            # predicted y
+            y_pred = np.dot(dataset.X, self.theta) + self.theta_zero
+
+            # computing and updating the gradient with the learning rate
+            gradient = (self.alpha * (1 / m)) * np.dot(y_pred - dataset.y, dataset.X)
+
+            # computing the penalty
+            penalization_term = self.alpha * (self.l2_penalty / m) * self.theta
+
+            # updating the model parameters
+            self.theta = self.theta - gradient - penalization_term
+            self.theta_zero = self.theta_zero - (self.alpha * (1 / m)) * np.sum(y_pred - dataset.y)
+
+        return self
